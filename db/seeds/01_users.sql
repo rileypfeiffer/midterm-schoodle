4d08ba7afd74131ecb584aeaaf1b325b04f5ffc6
Anthony <alasam@live.com>
2021-11-28 20:34:37 -0500

    Complete all seeds and schema. To be reviewed by mentor.

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,4 +3,7 @@ INSERT INTO users (email, name)
 VALUES ('bdoe@gmail.com', 'John Doe'),
 ('ssalmon@yahoo.com', 'Sally Salmon'),
 ('ppipper@gmail.com', 'Peter Pipper');
+('ppotts@gmail.com', 'Pepper Potts');
+('drodman@yahoo.com', 'Denis Rodman');
+('mjordan@aol.com', 'Michael Jordan');
 
--- db/seeds/03_responses.sql
+++ db/seeds/03_responses.sql
@@ -1,3 +1,11 @@
 -- Users table seeds here (Example)
-INSERT INTO responses (email, name)
-VALUES ('bdoe@gmail.com', 'John Doe'),
+INSERT INTO responses (event_id, responder_id, timeslot_response1, timeslot_response2, timesslot_reponse3)
+VALUES (1, 2, TRUE, TRUE, FALSE),
+VALUES (1, 4, TRUE, FALSE, FALSE),
+VALUES (1, 6, FALSE, TRUE, FALSE),
+VALUES (2, 3, FALSE, FALSE, FALSE),
+VALUES (2, 4, TRUE, TRUE, TRUE),
+VALUES (2, 5, FALSE, TRUE, FALSE),
+VALUES (3, 1, TRUE, FALSE, TRUE),
+VALUES (3, 5, TRUE, TRUE, FALSE),
+VALUES (3, 6, TRUE, TRUE, FALSE),
