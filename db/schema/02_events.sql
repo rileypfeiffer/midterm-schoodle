4b6ce74f831347a54b064acd08fdbfea85123333
Hamza Warsame <hamza360123@gmail.com>
2021-11-29 13:57:16 -0500

    update db

--- db/schema/02_events.sql
+++ db/schema/02_events.sql
@@ -5,7 +5,7 @@ CREATE TABLE events (
   id SERIAL PRIMARY KEY NOT NULL,
   organizer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   url VARCHAR(255) NOT NULL,
-  location VARCHAR (255) NOT NULL,
+  location VARCHAR(255) NOT NULL,
   date DATE NOT NULL,
   description TEXT NOT NULL,
   timeslot BOOLEAN NOT NULL
