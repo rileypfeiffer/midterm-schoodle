a7b5d4f3fd82db428cff0635787ae139651c8d31
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-11-29 20:09:11 -0500

    Add client script for creating invitations

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,25 @@
 // Client facing scripts here
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'midterm'
+});
+
+
+$(document).ready(function() {
+  const createInvitation = function(url) {
+    const values = [url]
+    const queryString = `SELECT * FROM events WHERE url = $1`
+    return pool
+      .query(queryString, values)
+      .then((result) => {
+        return result.rows[0];
+      })
+      .catch((result) => {
+        console.log(err.message);
+      });
+  };
+});
+
+exports.createInvitation = createInvitation;
