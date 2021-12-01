40ef95ab181719784d8d77122a9b80fd29a45332
Anthony <alasam@live.com>
2021-12-01 17:03:18 -0500

    changed vairables in random urls

--- routes/random_url.js
+++ routes/random_url.js
@@ -6,23 +6,16 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     fetchAPI("events").then((apiData) => {
 
-      let eventTitle = ""
-      let eventLocation = ""
-      let eventDate = ""
-      let eventtimeslot1 = ""
-      let eventtimeslot2 = ""
-      let eventtimeslot3 = ""
+
       let urlLookup = '3MshqBBNTOUknZmD3CDJ';
       // console.log(apiData);
       for (let data of apiData.events) {
         console.log("THISONE >>>>>>>>>", data.date);
         if (data.url === urlLookup) {
-          eventTitle = data.title
-          eventLocation = data.location
-          eventDate = data.date
-          eventtimeslot1 = data.timeslot1
-          eventtimeslot2 = data.timeslot2
-          eventtimeslot3 = data.timeslot3
+
+          //****** use data.____ for variables */
+
+
         } else {
           res.render("error");
         }
