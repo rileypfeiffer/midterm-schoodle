b5476cef4514f1de5e716fa9720add5e20474bf1
Anthony <alasam@live.com>
2021-12-02 21:13:08 -0500

    work on page refresh

--- public/scripts/event-popup.js
+++ public/scripts/event-popup.js
@@ -1,44 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.popup').hide()
   $('#createdURL').hide();
   $('.fa-clipboard').hide();
-  $('#event-form').submit(function(event) {
-          //post the data to the form
-      const $data = $(this).serialize();
-      event.preventDefault();
-      console.log($data)
-      $.ajax({
-          url:"/new-event",
-          data: $data,
-          method: "POST",
-          success: function(result){
-             console.log(result)
-            //  $('#url-slidedown').text(result.data);
-            $('#new-event').hide();
-            // $('#event-form').hide();
-            // $('#create-event-label').hide();
-            // $('#create-event-button').hide();
-             $('.popup').show();
-             $('#createdURL').show();
-             $('.fa-clipboard').show();
-            //  $('#url-slidedown').text(`URL CREATED, localhost:8080/invite/${result.data}`);
-            //  $('#url-slidedown').css({"visibility":"visible","display":"block"});
-             $('#createdURL').text(`URL CREATED! Press here to be redirected, or press the clipboard to copy`)
-             $('#createdURL').attr('href', `/invite/${result.data}`)
-             console.log('>>>>>>>>>>>>>>HELLO WORLD', $('#createdURL').text());
-            // setTimeout(() => {
-            //   window.location.href = `/invite/${result.data}`
-            // }, 5000)
-          },
-          error: function(error){
+  $('#event-form').submit(function (event) {
+    //post the data to the form
+    const $data = $(this).serialize();
+    event.preventDefault();
+    console.log($data)
+    $.ajax({
+      url: "/new-event",
+      data: $data,
+      method: "POST",
+      success: function (result) {
+        console.log(result)
+        $('#new-event').hide();
+        $('.popup').show();
+        $('#createdURL').show();
+        $('.fa-clipboard').show();
+        $('#createdURL').text(`URL CREATED! Press here to be redirected, or press the clipboard to copy`)
+        $('#createdURL').attr('href', `/invite/${result.data}`)
+        console.log('>>>>>>>>>>>>>>HELLO WORLD', $('#createdURL').text());
 
-          }
+      },
+      error: function (error) {
 
+      }
 
-      })
-  })
-
-    $('.fa-clipboard').click(function(){
 
     })
+  })
+
+  $('.fa-clipboard').click(function () {
+
+  })
 });
--- routes/invitation_page.js
+++ routes/invitation_page.js
@@ -50,7 +50,6 @@ module.exports = (db) => {
               console.log(error);
             })
 
-        res.redirect(`/:url`);
 
   });
 
